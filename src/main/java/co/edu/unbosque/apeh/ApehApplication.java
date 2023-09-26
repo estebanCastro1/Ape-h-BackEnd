961fdcc1e7395cd010913a212b4da11c324afe74
estebanCastro1 <138599757+estebanCastro1@users.noreply.github.com>
2023-09-26 10:59:48 -0500

    Primer commit del esqueleto del back end del proyecto ape-h

---
+++ src/main/java/co/edu/unbosque/apeh/ApehApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.apeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApehApplication.class, args);
+	}
+
+}
